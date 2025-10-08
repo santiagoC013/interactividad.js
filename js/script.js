db4a9bf847dbee01711f5703ac8ba2fc76682175
santiago <sgarciaca@centro.edu.mx>
2025-10-08 10:57:25 -0600

    variable

---
+++ js/script.js
@@ -0,0 +1,11 @@
+//asi se declara una variable
+var nombre = 'Santiago';
+//asi se declara una constante
+let edad = 22;
+const PI = Math.PI;
+
+let esMayorDeEdad = edad >= 18;
+
+nombre = "Lydia";
+edad = 29;
+console.log('Hola ' + nombre);
