9fdb83ddd34b940b6a588f800e9f09b5c4c10504
santiago <sgarciaca@centro.edu.mx>
2025-10-13 20:15:30 -0600

    background picker

--- js/darkmode.js
+++ js/darkmode.js
@@ -1,5 +1,17 @@
 const toggleThemeBtn = document.getElementById('toggle-theme-btn');
+const colorPicker = document.getElementById('color-picker');
+const colorBackground = document.getElementById('background-picker')
+//document.body.classList.add('clase-ejemplo');
 
 toggleThemeBtn.addEventListener('click', function(){
-    alert("me diste click");
+    //alert("me diste click");
+    document.body.classList.toggle('dark');
 })
+
+colorPicker:addEventListener('change', function() {
+document.body.style.color = colorPicker.value;
+});
+
+colorBackground:addEventListener('change', function() {
+    document.body.style.background = colorBackground.value;
+    });
--- js/script.js
+++ js/script.js
@@ -6,14 +6,14 @@ const PI = Math.PI;
 
 let esMayorDeEdad = edad >= 18;
 
-nombre = "Lydia";
-edad = 29;
-console.log('Hola ' + nombre);
+//nombre = "Lydia";
+//edad = 29;
+//console.log('Hola ' + nombre);
 
-function saludar(_nombre="Mundo") {
-    alert("Hola " + _nombre);
-}
+//function saludar(_nombre="Mundo") {
+ //   alert("Hola " + _nombre);
+//}
 
-saludar();
-saludar(nombre);
-saludar("Nadia");
+//saludar();
+//saludar(nombre);
+//saludar("Nadia");
